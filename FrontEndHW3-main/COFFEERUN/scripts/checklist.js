bf6a860e3215eac85328d2d00a55f94d9df5b076
Janessa <61309969+janessa17@users.noreply.github.com>
2021-11-10 21:04:03 -0800

    fixed

--- FrontEndHW3-main/COFFEERUN/scripts/checklist.js
+++ FrontEndHW3-main/COFFEERUN/scripts/checklist.js
@@ -1,63 +1,71 @@
-(function (window) {
-    'use strict';
-    var App = window.App || {};
-    var $ = window.jQuery;
+(function(window) {
+  "use strict";
 
-    function CheckList(selector) {
-        if (!selector) {
-            throw new Error('No selector provided');
-        }
-        this.$element = $(selector);
-        if (this.$element.length === 0) {
-            throw new Error('Could not find element with selector: ' + selector);
-        }
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector) {
+    if (!selector) {
+      throw new Error("No selector provided");
     }
-    CheckList.prototype.addRow = function (coffeeOrder) {
-        this.removeRow(coffeeOrder.email);
-        var rowElement = new Row(coffeeOrder);
-        this.$element.append(rowElement.$element);
-    };
-    CheckList.prototype.removeRow = function (email) {
-        this.$element
-            .find('[value="' + email + '"]')
-            .closest('[data-coffee-order="checkbox"]')
-            .empty();
-    };
-    CheckList.prototype.addClickHandler = function (fn) {
-        this.$element.on('click', 'input', function (event) {
-            this.removeRow(event.target.value);
-            fn(event.target.value);
-        }.bind(this));
-    };
+    this.$element = $(selector);
+    if (this.$element.length === 0) {
+      throw new Error("Could not find element with selector: " + selector);
+    }
+  }
 
-    function Row(coffeeOrder) {
-        var $div = $('<div/>', {
-            'data-coffee-order': 'checkbox',
-            class: 'checkbox'
-        });
+  CheckList.prototype.addClickHandler = function(fn) {
+    this.$element.on("click", "input", function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
 
-        var $label = $('<label></label>');
+  CheckList.prototype.addRow = function(coffeeOrder) {
+    this.removeRow(coffeeOrder.emailAddress);
 
-        var $checkbox = $('<input></input>', {
-            type: 'checkbox',
-            value: coffeeOrder.email
-        });
+    var rowElement = new Row(coffeeOrder);
 
-        var description = coffeeOrder.size + ' ';
-        if (coffeeOrder.flavor) {
-            description += coffeeOrder.flavor + ' ';
-        }
-        description += coffeeOrder.coffee + ', ';
-        description += ' (' + coffeeOrder.email + ')';
-        description += ' [' + coffeeOrder.strength + 'x]';
+    this.$element.append(rowElement.$element);
+  };
 
-        $label.append($checkbox);
-        $label.append(description);
-        $div.append($label);
+  CheckList.prototype.removeRow = function(email) {
+    this.$element
+      .find("[value='" + email + "']")
+      .closest("[data-coffee-order='checkbox']")
+      .remove();
+  };
 
-        this.$element = $div;
+  function Row(coffeeOrder) {
+    var $div = $("<div></div>", {
+      "data-coffee-order": "checkbox",
+      "class": "checkbox"
+    });
+
+    var $label = $("<label></label>");
+
+    var $checkbox = $("<input></input>", {
+      type: "checkbox",
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = coffeeOrder.size + " ";
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + " ";
     }
 
-    App.CheckList = CheckList;
-    window.App = App;
+    description += coffeeOrder.coffee + ", ";
+    description += " (" + coffeeOrder.emailAddress + ")";
+    description += " [" + coffeeOrder.strength + "x]";
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+  }
+
+  App.CheckList = CheckList;
+  window.App = App;
 })(window);
